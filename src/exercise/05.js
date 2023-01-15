06b4ee13ec9e81e21baea4413404179ad2313449
Oliver Day <oliver.day.swe@gmail.com>
2023-01-15 11:49:38 -0800

    feat: add 'create custom styled box component' solution

--- src/exercise/05.js
+++ src/exercise/05.js
@@ -1,42 +1,31 @@
 // Styling
 // http://localhost:3000/isolated/exercise/05.js
 
-import * as React from 'react'
-import '../box-styles.css'
+import * as React from 'react';
+import '../box-styles.css';
 
-const smallBox = (
-    <div
-      className="box box--small"
-      style={{ fontStyle: 'italic', backgroundColor: 'lightblue' }}
-    >
-      small lightblue box
-    </div>
-  );
-const mediumBox = (
-    <div
-      className="box box--medium"
-      style={{ fontStyle: 'italic', backgroundColor: 'pink' }}
-    >
-      medium pink box
-    </div>
-  );
-const largeBox = (
+const Box = ({ className = '', style, ...otherProps }) => (
   <div
-    className="box box--large"
-    style={{ fontStyle: 'italic', backgroundColor: 'orange' }}
-  >
-    large orange box
-  </div>
+    className={`box ${className}`}
+    style={{ fontStyle: 'italic', ...style }}
+    {...otherProps}
+  />
 );
 
 function App() {
   return (
     <div>
-      {smallBox}
-      {mediumBox}
-      {largeBox}
+      <Box className="box--small" style={{backgroundColor: 'lightblue'}}>
+        small lightblue box
+      </Box>
+      <Box className="box--medium" style={{backgroundColor: 'pink'}}>
+        medium pink box
+      </Box>
+      <Box className="box--large" style={{backgroundColor: 'orange'}}>
+        large orange box
+      </Box>
     </div>
   )
 }
 
-export default App
+export default App;
