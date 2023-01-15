2df7a1a78629e4ea0f596fc3e66b0925cd4de5c0
Oliver Day <oliver.day.swe@gmail.com>
2023-01-15 14:35:50 -0800

    feat: add 'form basics validate lower-case' solution

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -5,19 +5,34 @@ import * as React from 'react';
 
 function UsernameForm({ onSubmitUsername }) {
   const usernameInputRef = React.useRef();
+  const [error, setError] = React.useState(null);
 
   const handleSubmit = (event) => {
     event.preventDefault();
     onSubmitUsername(usernameInputRef.current.value);
   };
 
+  const handleChange = (event) => {
+    const { value } = event.target;
+    const isLowerCase = value === value.toLowerCase();
+    setError(isLowerCase ? null : 'Username must be lower case');
+  };
+
   return (
     <form onSubmit={handleSubmit}>
       <div>
         <label htmlFor="usernameInput">Username:</label>
-        <input id="usernameInput" type="text" ref={usernameInputRef} />
+        <input
+          id="usernameInput"
+          type="text"
+          onChange={handleChange}
+          ref={usernameInputRef}
+        />
       </div>
-      <button type="submit">Submit</button>
+      <div role="alert" style={{ color: 'red' }}>
+        {error}
+      </div>
+      <button disabled={Boolean(error)} type="submit">Submit</button>
     </form>
   )
 }
